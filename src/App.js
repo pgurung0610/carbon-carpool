d9e680a768338d3e7ca18f71a69bd658840fab59
Victoria See <victoria2aug@gmail.com>
2020-02-15 09:57:10 -0500

    Created login and create event page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login.js'
+import React from "react";
+import "./App.css";
+import Login from "./components/Login.js";
+import CreateEvent from "./components/CreateEvent.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <CreateEvent />
       </header>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Login() {
   return (
     <div className="Login">
-      Hello World
+      User Name: &nbsp;
+      <input type="text" />
+      <text>
+        {" "}
+        <br />{" "}
+      </text>
+      Password: &nbsp;
+      <input type="text" />
+      <text>
+        {" "}
+        <br />{" "}
+      </text>
+      <button>Login</button>{" "}
     </div>
   );
 }
